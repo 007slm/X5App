39c8452873a27a73e462be48a13f43fd40e11f31
007slm <007slm@163.com>
2018-07-27 17:21:58 +0800

    first commit

---
+++ JustepX5ForIpad/JustepX5/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  JustepX5
+//
+//  Created by 007slm(007slm@163.com)
+//  JustepAppDelegate.h
+//  JustepX5
+//
+//  Created by 007slm on 12-6-5.
+//  Copyright (c) 2012年 Justep. All rights reserved.
+// on 12-6-5.
+//  Copyright (c) 2012年 Justep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JustepAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JustepAppDelegate class]));
+    }
+}
+	
